0102ca91ff3bd7286e1ea6ebc4e1f97588b06c7b
Damian Jackiewicz <damian.jackiewicz92@gmail.com>
2019-12-12 05:40:15 +0100

    Add if instruction to show and hide posts

--- js/app.js
+++ js/app.js
@@ -31,43 +31,41 @@ fetch("https://jsonplaceholder.typicode.com/users")
             const btn = clone.querySelector('.btn');
 
             btn.addEventListener('click', () => {
-                fetch(`https://jsonplaceholder.typicode.com/posts?userId=${user.id}`)
-                    .then(resp =>
-                        resp.json()
-                    )
-                    .then(resp => {
-                        resp.forEach(news => {
-                            // Clone post template content
-                            const clone = document.importNode(post.content, true);
-                            // Find element with class "post-title" in post template content
-                            const postTitle = clone.querySelector('.post-title');
-                            // Insert title of each post in API to h3
-                            postTitle.innerHTML = news.title;
-                            // Find element with class "post-body" in post template content
-                            const postBody = clone.querySelector('.post-body');
-                            // Insert body post of each post in API to div with class "post-body"
-                            postBody.innerHTML = news.body;
-                            // Change display value to show ulList
-                            let flag = true;
-
-                            if (ulList.dataset.visible === "false") {
-                                flag = !flag;
-                            } else if (ulList.dataset.visible === "true") {
-                                flag = !flag;
-                            }
-
-                            if (flag) {
-                                ulList.style.display = "none";
-                                btn.innerHTML = "Show posts";
-                                ulList.dataset.visible = "false"
-                            } else {
-                                ulList.dataset.visible = "true"
+                if (ulList.dataset.visible === "false") {
+                    fetch(`https://jsonplaceholder.typicode.com/posts?userId=${user.id}`)
+                        .then(resp =>
+                            resp.json()
+                        )
+                        .then(resp => {
+                            resp.forEach(news => {
+                                // Clone post template content
+                                const clone = document.importNode(post.content, true);
+                                // Find element with class "post-title" in post template content
+                                const postTitle = clone.querySelector('.post-title');
+                                // Insert title of each post in API to h3
+                                postTitle.innerHTML = news.title;
+                                // Find element with class "post-body" in post template content
+                                const postBody = clone.querySelector('.post-body');
+                                // Insert body post of each post in API to div with class "post-body"
+                                postBody.innerHTML = news.body;
+                                // Change display value to show ulList
                                 ulList.style.display = "block";
+                                // Change value of dataset-visible on true after click
+                                ulList.dataset.visible = "true";
                                 btn.innerHTML = "Hide posts";
-                            }
-                            ulList.appendChild(clone);
+                                // Add cloned template content to ul with clas users-posts
+                                ulList.appendChild(clone);
+                            })
                         })
-                    })
+                } else {
+                    // Change display value to hide ulList
+                    ulList.style.display = "none";
+                    // Change value of dataset-visible on false after click
+                    ulList.dataset.visible = "false"
+                    // Clear ul list
+                    ulList.innerHTML = "";
+                    btn.innerHTML = "Show posts"
+                }
             });
 
             // Add cloned template content to div with id #usersList
